999f48399883aced4683ec14207a4f1c8a200c9a
EC2 Default User <ec2-user@ip-172-31-4-70.us-east-2.compute.internal>
2018-10-23 05:53:12 +0000

    Jee_upload

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,10 +1,12 @@
 class Question < ActiveRecord::Base
-    attr_accessible :keyword, :answer, :weight
+    attr_accessible :keyword, :answer, :weight, :index
     
     
     def self.upload(file)
         CSV.foreach(file.path, headers: true) do |row|
-            Product.create! row.to_hash
+            question = find_by_id(row["index"]) || new
+            question.attributes = row.to_hash.slice(*accessible_attributes)
+            product.save!
         end
     end
 end
