4a7998deff11de3255340d805fc1a91aab6717e3
EC2 Default User <ec2-user@ip-172-31-4-70.us-east-2.compute.internal>
2018-10-25 06:13:15 +0000

    d

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,21 +1,15 @@
 class Question < ActiveRecord::Base
 
     def self.upload(file)
-        accessible_attributes = ['keyword','index', 'answer', 'weight']
         CSV.foreach(file.path, headers: true) do |row|
-            question = find_by_id(row["index"]) || new
-            question.attributes = row.to_hash.slice(*accessible_attributes)
-            product.save!
+            question_hash = row.to_hash
+            question = self.where(id: question_hash["index"])
+            if question.present?
+                question.first.update_attributes(question_hash)
+            else
+                self.create!(question_hash)
+            end
+            
         end
     end
-#     def self.import(file)
-#     accessible_attributes = ['my_attribute_name1','my_attribute_name2', '...']
-#     spreadsheet = open_spreadsheet(file)
-#     header = spreadsheet.row(1)
-#     (2..spreadsheet.last_row).each do |i|
-#       row = Hash[[header, spreadsheet.row(i)].transpose]
-#       cae = find_by_id(row["id"]) || new
-#       cae.attributes = row.to_hash.slice(*accessible_attributes)
-#       cae.save!
-#   end
 end
