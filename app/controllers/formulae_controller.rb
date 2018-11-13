b42766eddb3dac68852101e30c04b2e03550a9f6
mohthorn <mohthorn@gmail.com>
2018-11-13 05:52:18 +0000

    preliminary formulae

--- app/controllers/formulae_controller.rb
+++ app/controllers/formulae_controller.rb
@@ -5,5 +5,7 @@ class FormulaeController < ApplicationController
         @input_nav_class = '' # Input Tab
         @formulae_nav_class = 'active'
         @output_nav_class = ''
+        
+        @subcategories = Subcategory.all
     end
 end
