7e7e1c9f519d0ce8f840d2e6a6b8a1eb023a0ce3
mohthorn <mohthorn@gmail.com>
2018-12-11 22:11:25 +0000

    reducing stupid queries: performance gain

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,17 +2,6 @@ class Answer < ActiveRecord::Base
      belongs_to :question
      belongs_to :company
      
-     def self.find_val(company_id, question_id)
-         answer = Answer.find_by(question_id: question_id, company_id: company_id)
-         return answer.validated
-     end
-
-
-     def self.find_level(company_id, question_id)
-          answer = Answer.find_by(question_id: question_id, company_id: company_id) 
-          return answer.level
-     end  
-     
      def self.upload(file,company_id)
         #csv=CSV.parse(file.path,headers: true,skip_blanks: true).reject { |row| row.to_hash.values.all?(&:nil?) }
         CSV.foreach(file.path, headers: true) do |row|
